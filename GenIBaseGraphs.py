786cb4b25cd7540e6d8fc4b21727f504c3b805c5
pandp <pythonandpokemon@gmail.com>
2015-02-17 21:50:16 -0500

    Add comments

--- GenIBaseGraphs.py
+++ GenIBaseGraphs.py
@@ -1,10 +1,15 @@
+# imports
 import matplotlib.pyplot as plt;
 import pandas
 import numpy
 import seaborn as sns
 
+# Read in the data
 BaseStats = pandas.read_csv("C:\PandP\GenIBaseStats\GenIBaseStats.csv")
 Types = pandas.read_csv("C:\PandP\GenITypes\GenITypes.csv")
+# Combine the data together so that each Pokemon's types are represented
+# If a Pokemon has two types, it is listed twice
+# Just stack the sets
 Data = pandas.merge(BaseStats,Types.drop('Name',axis=1),on="Number")
 Type1 = Data.drop("Type2",axis=1)
 Type1.rename(columns={"Type1":"Type"}, inplace = True)
@@ -13,19 +18,23 @@ Type2.rename(columns={"Type2":"Type"}, inplace = True)
 Type2 = Type2[pandas.notnull(Type2['Type'])]
 Data = Type1.append(Type2)
 
+# Save the list of types
 types = []
+# Restructure the data
 HPbyType = numpy.empty(len(Data['Type'].unique()), dtype=pandas.Series)
 for i, type in enumerate(sorted(Data['Type'].unique())):
 	types.append(type)
 	HPbyType[i]=pandas.Series(Data['HP'][Data['Type']==type],name=str(type))
 
-ind = numpy.arange(1,len(Data['Type'].unique())+1)
-
-
+# Max value so that all the plots are on the same scale
 maxVal = Data.ix[:,2:7].max().max()
 
+# Make the plot
+# and add plot features
+print("HP")
 sns.boxplot(vals=HPbyType)
 plt.ylim((0,maxVal))
+ind = numpy.arange(1,len(Data['Type'].unique())+1)
 plt.xticks(ind, types)
 plt.xticks(rotation=70)
 plt.xlabel('Types')
@@ -36,7 +45,7 @@ plt.tight_layout()
 plt.savefig("C:\PandP\GenIBaseStats\BaseGraphs\HP.pdf")
 plt.show()
 
-
+# Rerun through the other stats
 for stat in Data.columns[3:7]:
 	print(stat)
 	plt.clf()
